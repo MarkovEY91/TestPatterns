2365506d4c12ac3000aa881f05f8b16434d60200
MarkovEY91 <markovewgen@gmail.com>
2024-10-10 11:41:05 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using TestPatterns.Порождающие.Singleton;
+
+namespace TestPatterns
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Singleton singleton1 = new Singleton();
+            //Singleton s1 = Singleton.GetInstance();
+            //Singleton s2 = Singleton.GetInstance();
+
+            //if (s1 == s2)
+            //{
+            //    Console.WriteLine($"s1 ({s1.GetHashCode()}) это s2 ({s2.GetHashCode()}) ");
+            //}
+
+            //s1.SomeBuisnessLogic();
+
+
+
+            MyForm myForm1 = MyForm.GetInstance();
+            MyForm myForm2 = MyForm.GetInstance();
+
+            Thread.Sleep(3000);
+
+            myForm2.SetWidth(1000);
+
+
+
+            Console.Read();
+        }
+    }
+}
---
+++ Порождающие/Singleton/MyForm.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestPatterns.Порождающие.Singleton
+{
+    public sealed class MyForm: Form
+    {
+        private static MyForm _instance;
+        private MyForm() { }
+
+        public static MyForm GetInstance()
+        {
+            if (_instance == null)
+            {
+                _instance = new MyForm();
+                _instance.Width = 200;
+                _instance.Height = 200;
+                _instance.Show();
+            }
+
+            return _instance;
+        }
+
+        public void SetWidth(int width)
+        {
+            _instance.Width = width;    
+
+            
+        }
+    }
+}
